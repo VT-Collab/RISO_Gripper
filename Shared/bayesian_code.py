69084734c7e999899b3a1b660aaa178f08ebe157
mayanicolek <131313635+mayanicolek@users.noreply.github.com>
2023-10-04 15:56:37 -0400

    Update bayesian_code.py

--- Shared/bayesian_code.py
+++ Shared/bayesian_code.py
@@ -41,20 +41,21 @@ data = {"Time": [], "Position": [], "Force": [], "Inputs": [], "Voltage": []}
 # Initialize Variables
 interface = Joystick()
 voltage = 8.1
+scaleh = 0.5
+des_force = -15
 send_arduino(comm_arduino, str(voltage))
 send2gripper(conn_gripper, 'o')
 time.sleep(2)
-
-input('Press Enter to Start Trial')
-scaleh = 0.5
+# Boolean variables for mode switching
 choose = True
 pick = False
-des_force = -15
 
+input('Press Enter to Start Trial')
 
 while True:
 
     # SHARED AUTONOMOUS MODE: pick up object
+    
     while True:
         z, A_pressed, B_pressed, X_pressed, Y_pressed, START_pressed, STOP_pressed, RT, LT = interface.input()
         Joystick_inputs = [z, A_pressed, B_pressed, X_pressed, Y_pressed, START_pressed, STOP_pressed, RT, LT]
@@ -62,7 +63,8 @@ while True:
         cur_pos, wrench = find_pos(conn)
         x_dot = [0.] * 6
         data = append_data(data, time.time(), cur_pos, wrench, voltage, Joystick_inputs)
-        
+
+        # before picking an object: choose gripper and take picture of objects
         if choose:
             if A_pressed or B_pressed:
                 choose = False
@@ -90,13 +92,8 @@ while True:
                 # define robot's prior belief over objects
                 prior = [1/(len(objects))] * len(objects)
                 timestep = time.time()
-
-        if START_pressed:
-            pickle.dump(data, open(file_name, 'wb'))
-            print("DATA SAVED")
-            send_arduino(comm_arduino, 8.1)
-            exit()
-
+        
+        # shared motion towards object based on probability distribution
         if pick:
             if time.time() - timestep > 0.5:
                 P = predict_goal(data['Position'][0][0:3], cur_pos[0:3], aH[0:3], objects, prior)
@@ -104,18 +101,30 @@ while True:
                 alpha = get_alpha(P)
                 aR = get_assist(cur_pos[0:3], objects, P)
                 print(P)
+
+                # shared autonomy equations
                 x_dot[0] = (1-alpha)*scaleh*aH[0] + alpha*aR[0]
                 x_dot[1] = (1-alpha)*scaleh*aH[1] + alpha*aR[1]
                 x_dot[2] = (1-alpha)*scaleh*aH[2] + alpha*aR[2]
                 run_xdot(x_dot, conn)
 
-        
+        # move from shared control to autonomous control
         if STOP_pressed:
             choose = True
             pick = False
             break
+            
+        # end trial
+        if START_pressed:
+            pickle.dump(data, open(file_name, 'wb'))
+            print("DATA SAVED")
+            send_arduino(comm_arduino, 8.1)
+            exit()
 
+    
     # FULLY AUTON MODE: drop object in basket
+
+    # pick objects using preset picking procedure
     if obj == 'rigid':
         send2gripper(conn_gripper, "c")
         time.sleep(2)
@@ -141,34 +150,34 @@ while True:
                     run_xdot(x_dot, conn)
                 break
             run_xdot(x_dot, conn)
-            
 
+    # move up for 4 seconds to avoid other objects
     timestep = time.time()
     while time.time() - timestep < 4:
         xdot = [0., 0., 0.1, 0., 0., 0.]
         run_xdot(xdot, conn)
 
+    # move to basket
     cur_pos, wrench = find_pos(conn)
     basket_pos = [0.45, -0.68, 0.28, cur_pos[3], cur_pos[4], cur_pos[5]]
     make_traj(cur_pos, basket_pos, 5)
     data = play_shared_traj(conn, data, 'traj.pkl', voltage, 5)
-    
+
+    # drop object in basket
     if obj == 'rigid':
         send2gripper(conn_gripper, 'o')
         time.sleep(2)
 
     if obj == 'soft':
         voltage = 8.4
-        send2gripper(conn_gripper, 'o')
-        time.sleep(2)
         send_arduino(comm_arduino, voltage)
         time.sleep(10)
 
     print('object dropped in basket')
 
+    # move to home location and prepare soft gripper
     if obj == 'soft':
         voltage = 2.0
         send_arduino(comm_arduino, voltage)
-
     go2home(conn, h=HOME)
     print('ready to pick up next object')
