7fd925620fb1b3e426273e2f328299c396327a37
mayanicolek <131313635+mayanicolek@users.noreply.github.com>
2023-10-04 12:24:55 -0400

    Update camerapractice.py

--- Autonomous/camerapractice.py
+++ Autonomous/camerapractice.py
@@ -12,20 +12,10 @@ from multiprocessing import Process
 from utils import connect2robot, go2home, readState, play_traj,\
 	get_target, send_force, teleop, connect2pressure,\
 	connect2gripper, send2gripper
-HOME = [0.0, -0.0, -0.0, -1.28331, -0.0, 1.27004, 0.756409]
 
-# PORT_robot = 8080
-# print('[*] Connecting to low-level controller...')
-# conn = connect2robot(PORT_robot)
-# print("Connection Established")
-
-#go2home(conn, h=HOME)
-#print("Returning Home")
-
+# observe camera frame without connecting to robot
 while True:
     input("Observe object")
     x, y, z, obj = get_target()
     print("X:", y,"Y:", x,"Z:", z, obj)
-    # states = readState(conn)
-    # print(states['x'])
     
